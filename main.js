90d8ab8fa33a11589db23c6677c17053bdd4ee7b
ClaPascarella <claudiopascarella1996@gmail.com>
2024-04-18 23:43:44 +0200

    file css

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-let percorrenza = prompt ("quanti chilometri percorri?")
-console.log( `km percorsi: ${percorrenza}` );
+// let percorrenza = prompt ("quanti chilometri percorri?")
+// console.log( `km percorsi: ${percorrenza}` );
 
-let età = prompt ("quanti anni hai?")
-console.log(`la tua eta è ${età}`);
+// let età = prompt ("quanti anni hai?")
+// console.log(`la tua eta è ${età}`);
 
-let  prezzoAlKilometro = 0.21;
-const prezzoBigliettoSenzaSconto = prezzoAlKilometro * percorrenza;
-const scritto = "Il prezzo del biglietto è "
-const euro = " Euro"
-console.log(prezzoBigliettoSenzaSconto);
+// let  prezzoAlKilometro = 0.21;
+// const prezzoBigliettoSenzaSconto = prezzoAlKilometro * percorrenza;
+// const scritto = "Il prezzo del biglietto è "
+// const euro = " Euro"
+// console.log(prezzoBigliettoSenzaSconto);
 
 
 
-   const myButton = document.getElementById('button-click');
-   myButton.addEventListener('click', fuction() 
+//    const myButton = document.getElementById('button-click');
+//    myButton.addEventListener('click', fuction() 
 
-   );
+//    );
